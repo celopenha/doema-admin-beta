a85055715cdc99a4ae3a1a2fed9e53dd754fef6f
Raul Correia <raulvinicius245@gmail.com>
2021-02-11 09:42:22 -0300

    Criado CRUD para usuario

--- app/routers/usuario.js
+++ app/routers/usuario.js
@@ -19,67 +19,48 @@ let teste;
 //export const list2 = "teste";
 
 
-module.exports = async function (app) {
+
+module.exports = async function(app) {
 
     app.use(cookieParser());
     app.use(session({ secret: "2C44-4D44-WppQ38S" }));
 
-    app.use(require('connect-flash')());
-    app.use(function (req, res, next) {
-        res.locals.messages = require('express-messages')(req, res);
-        next();
-    });
-
     // Rota para exibição da View Listar
-    app.get('/app/' + rota + '/list', function (req, res) {
+    app.get('/app/' + rota + '/list', function(req, res) {
 
         if (!req.session.token) {
             res.redirect('/app/login');
-        } else if (req.session.usuario.niveis[0] != 'ADMIN') {
-            res.redirect('/');
         } else {
             teste = request({
-                url: process.env.API_HOST + rota + "/nome/nivel/0/10",
+                url: process.env.API_HOST + rota,
                 method: "GET",
                 json: true,
                 headers: {
                     "content-type": "application/json",
                     "Authorization": req.session.token
                 },
-            }, function (error, response, body) {
-                if (response.statusCode != 200) {
-                    req.flash("danger", "Ops, ocorreu um erro.");
-                    req.session.destroy();
-                    res.redirect('/app/login');
-                }
+            }, function(error, response, body) {
                 lista = [];
-
-                for (var i = 0; i < Object.keys(body.data.content).length; i++) {
-                    if (body.data.content[i].contato1 == null || body.data.content[i].contato1 == '') {
-                        body.data.content[i].contato1 = body.data.content[i].contato2
-                    }
+                for (var i = 0; i < Object.keys(body.data).length; i++) {
                     const finallista = {
-                        id: body.data.content[i].id,
-                        nome: body.data.content[i].nome,
-                        username: body.data.content[i].username,
-                        niveis: body.data.content[i].niveis,
-                        ativo: body.data.content[i].ativo,
-                        contato1: body.data.content[i].contato1, //telefone
-                        contato2: body.data.content[i].contato2, //celular
-                        email: body.data.content[i].email
-
+                        id: body.data[i].id,
+                        nome: body.data[i].nome,
+                        username: body.data[i].username,
+                        niveis: body.data[i].niveis,
+                        ativo: body.data[i].ativo,
+                        telefone: body.data[i].telefone,
+                        email: body.data[i].email
                     };
                     lista.push(finallista);
                 }
                 res.format({
-                    html: function () {
-                        res.render(rota + '/List', {
+                    html: function() {
+                        res.render(rota + '/List', { 
                             itens: lista,
                             page: rota,
                             informacoes: req.session.json,
                             number: body.data.number,
                             totalPages: body.data.totalPages,
-                            jsonescola: req.session.jsonescola
                         });
 
                     }
@@ -91,67 +72,68 @@ module.exports = async function (app) {
     });
 
     // Rota para exibição da View Criar
-    app.get('/app/' + rota + '/create', function (req, res) {
+    app.get('/app/' + rota + '/create', function(req, res) {
         if (!req.session.token) {
             res.redirect('/app/login');
         } else {
             res.format({
-                html: function () {
-                    res.render(rota + '/Create', { page: rota, informacoes: req.session.json, jsonescola: req.session.jsonescola });
+                html: function() {
+                    res.render(rota + '/Create', { page: rota, informacoes: req.session.json });
                 }
             });
         }
     });
 
     // Rota para receber parametros via post criar item
-    app.post('/app/' + rota + '/create/submit', upload.single('photo'), function (req, res) {
-
-        const file = req.file;
-        let foto;
-        if (file) {
-            const buf = Buffer.from(req.file.buffer);
-            foto = buf.toString('base64');
-        } else {
-            foto = process.env.PROFILE_IMG
-        }
-        request({
-            url: process.env.API_HOST + rota,
-            method: "POST",
-            json: true,
-            headers: {
-                "content-type": "application/json",
-                "Authorization": req.session.token
-            },
-            json: {
-                "nome": req.body.nome,
-                "username": req.body.username,
-                "password": req.body.password,
-                "niveis": ["ADMIN"],
-                "ativo": req.body.ativo,
-                "habilitado": true,
-                "expirado": false,
-                "bloqueado": false,
-                "contato1": req.body.contato1,
-                "contato2": req.body.contato2,
-                "email": req.body.email,
-                "redefinir" : false
-            },
-        }, function (error, response, body) {
-
-            if (response.statusCode != 200) {
-                req.flash("danger", "Não foi possível criar usuário. " + body.errors);
-            } else {
-                req.flash("success", "Usuário criado com sucesso.");
-            }
-
+    app.post('/app/' + rota + '/create/submit', upload.single('photo'), function(req, res) {
+       /* if (req.file.size > 220220) {
+            req.flash("danger", "Item não salvo. Sua imagem deve ter até 200kb.");
             res.redirect('/app/' + rota + '/list');
-            return true;
-        });
+        } else {
+            const file = req.file;
+            let foto;
+            if (file) {
+                const buf = Buffer.from(req.file.buffer);
+                foto = buf.toString('base64');
+            } else {
+            foto = process.env.PROFILE_IMG
+            }*/
+            request({
+                url: process.env.API_HOST + rota,
+                method: "POST",
+                json: true,
+                headers: {
+                    "content-type": "application/json",
+                    "Authorization": req.session.token
+                },
+                json: {
+                    "nome": req.body.nome,
+                    "username": req.body.username,
+                    "password": req.body.password,
+                    "niveis": ["ADMIN"],
+                    "ativo": req.body.ativo,
+                    "habilitado": true,
+                    "expirado": false,
+                    "bloqueado": false,
+                    "telefone": req.body.telefone,
+                    "email": req.body.email
+                },
+            }, function(error, response, body) {
 
-    });
+                if (response.statusCode != 200) {
+                    req.flash("danger", "Não foi possível criar usuário. " + body.errors);
+                } else {
+                    req.flash("success", "Usuário criado com sucesso.");
+                }
+
+                res.redirect('/app/' + rota + '/list');
+                return true;
+            });
+        });
+    
 
     // Rota para exibição da View Editar
-    app.get('/app/' + rota + '/edit/:id', function (req, res) {
+    app.get('/app/' + rota + '/edit/:id', function(req, res) {
         if (!req.session.token) {
             res.redirect('/app/login');
 
@@ -164,26 +146,21 @@ module.exports = async function (app) {
                     "content-type": "application/json",
                     "Authorization": req.session.token
                 },
-            }, function (error, response, body) {
-                if (response.statusCode != 200) {
-                    req.flash("danger", "Ops, ocorreu um erro.");
-                    req.session.destroy();
-                    res.redirect('/app/login');
-                }
+            }, function(error, response, body) {
+
                 res.format({
-                    html: function () {
+                    html: function() {
                         res.render(rota + '/Edit', {
                             id: body.data.id,
                             nome: body.data.nome,
                             username: body.data.username,
                             password: body.data.password,
-                            nivel: body.data.niveis,
                             page: rota,
                             ativo: body.data.ativo,
-                            contato1: body.data.contato1,
-                            contato2: body.data.contato2,
+                            telefone: body.data.telefone,
                             email: body.data.email,
-                            informacoes: req.session.json, jsonescola: req.session.jsonescola
+                            number: body.data.number,
+                            informacoes: req.session.json
                         });
                     }
                 });
@@ -195,46 +172,37 @@ module.exports = async function (app) {
     });
 
     // Rota para receber parametros via post editar item
-    app.post('/app/' + rota + '/edit/submit', upload.single('photo'), function (req, res) {
+    app.post('/app/' + rota + '/edit/submit', upload.single('photo'), function(req, res) {  
+        
+            request({
+                url: process.env.API_HOST + rota,
+                method: "PUT",
+                json: true,
+                headers: {
+                    "content-type": "application/json",
+                    "Authorization": req.session.token
+                },
+                json: {
+                    "id": req.body.id,
+                    "nome": req.body.nome,
+                    "username": username,
+                    "niveis": nivel,
+                    "telefone": req.body.telefone,
+                    "email": req.body.email,
+                    "ativo": req.body.ativo,
+                },
+            }, function(error, response, body) {
 
-        const file = req.file;
-        let foto;
-        if (file) {
-            const buf = Buffer.from(req.file.buffer);
-            foto = buf.toString('base64');
-        } else {
-            foto = imagem;
-        }
-        request({
-            url: process.env.API_HOST + rota,
-            method: "PUT",
-            json: true,
-            headers: {
-                "content-type": "application/json",
-                "Authorization": req.session.token
-            },
-            json: {
-                "id": req.body.id,
-                "nome": req.body.nome,
-                "username": req.body.username,
-                "niveis": req.body.nivel,
-                "contato1": req.body.contato1,
-                "contato2": req.body.contato2,
-                "email": req.body.email,
-                "ativo": req.body.ativo,
-            },
-        }, function (error, response, body) {
+                if (response.statusCode != 200) {
+                    req.flash("danger", "Não foi possível alterar o item. " + body.errors);
+                } else {
+                    req.flash("success", "Item alterado com sucesso.");
+                }
 
-            if (response.statusCode != 200) {
-                req.flash("danger", "Não foi possível alterar o item. " + body.errors);
-            } else {
-                req.flash("success", "Item alterado com sucesso.");
-            }
-
-            res.redirect('/app/' + rota + '/list');
-            return true;
-        });
-
+                res.redirect('/app/' + rota + '/list');
+                return true;
+            });
+        
     });
 
     // Rota para exclusão de um item
@@ -265,7 +233,7 @@ module.exports = async function (app) {
         }
     });
 
-
+    
     // Rota para exibição da View Perfil
     app.get('/app/' + rota + '/perfil', function (req, res) {
         if (!req.session.token) {
@@ -281,11 +249,7 @@ module.exports = async function (app) {
                     "Authorization": req.session.token
                 },
             }, function (error, response, body) {
-                if (response.statusCode != 200) {
-                    req.flash("danger", "Ops, ocorreu um erro.");
-                    req.session.destroy();
-                    res.redirect('/app/login');
-                }
+
                 res.format({
                     html: function () {
                         res.render(rota + '/Perfil', {
@@ -299,10 +263,9 @@ module.exports = async function (app) {
                             habilitado: body.data.habilitado,
                             expirado: body.data.expirado,
                             bloqueado: body.data.bloqueado,
-                            contato1: body.data.contato1,
-                            contato2: body.data.contato2,
+                            telefone: body.data.telefone,
                             email: body.data.email,
-                            informacoes: req.session.json, jsonescola: req.session.jsonescola
+                            informacoes: req.session.json,
                         });
                     }
                 });
@@ -330,8 +293,7 @@ module.exports = async function (app) {
                 "nome": req.body.nome,
                 "username": username,
                 "niveis": nivel,
-                "contato1": req.body.contato1,
-                "contato2": req.body.contato2,
+                "telefone": req.body.telefone,
                 "email": req.body.email,
                 /*
                 "ativo": req.body.ativo,
@@ -349,65 +311,14 @@ module.exports = async function (app) {
                 req.session.json.NomeUser = body.data.nome;
             }
 
-            res.redirect('/');
+            res.redirect('/app/usuario/list');
             return true;
         });
 
     });
 
-    app.post('/app/' + rota + '/list', function (req, res) {
-        if (!req.session.token) {
-            res.redirect('/app/login');
-        } else if (req.session.usuario.niveis[0] != 'ADMIN') {
-            res.redirect('/');
-        } else { // " + req.body.size + "
-            request({
-                url: encodeURI(process.env.API_HOST + rota + "/nome/nivel/" + req.body.page + "/" + req.body.size + "?nome=" + req.body.busca),
-                method: "GET",
-                json: true,
-                headers: {
-                    "content-type": "application/json",
-                    "Authorization": req.session.token
-                },
-            }, function (error, response, body) {
-                if (response.statusCode != 200) {
-                    req.flash("danger", "Ops, ocorreu um erro.");
-                    res.redirect("/");
-                }
-                lista = [];
-                for (var i = 0; i < Object.keys(body.data.content).length; i++) {
-                    if (body.data.content[i].contato1 == null || body.data.content[i].contato1 == '') {
-                        body.data.content[i].contato1 = body.data.content[i].contato2
-                    }
-                        const finallista = {
-                            id: body.data.content[i].id,
-                            nome: body.data.content[i].nome,
-                            username: body.data.content[i].username,
-                            niveis: body.data.content[i].niveis,
-                            ativo: body.data.content[i].ativo,
-                            contato1: body.data.content[i].contato1, //telefone
-                            contato2: body.data.content[i].contato2, //celular
-                            email: body.data.content[i].email
-
-                        };
-                        lista.push(finallista);
-                }
-
-                return res.json({
-                    itens: lista,
-                    page: rota,
-                    informacoes: req.session.json,
-                    number: body.data.number,
-                    totalPages: body.data.totalPages
-                });
-
-            });
-
-        }
-    });
-
-    // Rota para exibição da View Alterar Senha
-    app.get('/app/' + rota + '/alterar-senha', function (req, res) {
+     // Rota para exibição da View Alterar Senha
+     app.get('/app/' + rota + '/alterar-senha', function (req, res) {
         if (!req.session.token) {
             res.redirect('/app/login');
         } else {
@@ -415,7 +326,6 @@ module.exports = async function (app) {
                 html: function () {
                     res.render(rota + '/Alterar-Senha', {
                         page: rota,
-                        jsonescola: req.session.jsonescola,
                         informacoes: req.session.json
                     });
                 }
@@ -458,4 +368,5 @@ module.exports = async function (app) {
             });
         }
     })
+
 }
