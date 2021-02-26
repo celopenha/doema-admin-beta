ed30ebb1fcf657a4aaeec70c0840726d9cdfa4ad
Marcelo Penha <niqcelo@gmail.com>
2021-02-26 09:41:31 -0300

    Estilização de mensagens flash

--- app/routers/public.js
+++ app/routers/public.js
@@ -2,7 +2,17 @@ const rota = require('path').basename(__filename, '.js');
 
 
 module.exports = async (app) => {
-    app.get('/public', (req, res) => {
-        res.render(`${rota}/index`);
-    })
+	app.get('/public', (req, res) => {
+		res.render(`${rota}/index`);
+	});
+
+	app.get('/public/biblioteca', (req, res) => {
+		res.render('public/Biblioteca');
+	});
+
+	app.post('/public/jornal/listar', (req, res) => {
+		console.log(req.body);
+
+		res.render('public/JornalList');
+	});
 }
