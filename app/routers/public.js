387c6ea9420812e088666a8c2bc932b6a20050f0
Marcelo Penha <niqcelo@gmail.com>
2021-03-17 15:00:16 -0300

    refatoração

--- app/routers/public.js
+++ app/routers/public.js
@@ -1,45 +1,57 @@
 const rota = require('path').basename(__filename, '.js');
-const moment = require('moment');
 const axios = require('axios').default;
+var moment = require('moment');
+
 
 module.exports = async (app) => {
   app.get('/inicio', async (req, res) => {
-    // url do ENDPOINT P/ RESGATAR A ÚLTIMA MENSAGEM CADASTRADA DIRETAMENTE NA API
+
+    // URL'S PARA BUSCAR DADOS (MENSAGEM E FERIADOS) NA API
     const msgUrl = `${process.env.API_HOST}mensagem/ultima-mensagem`;
-    // REQUISIÇÃO À API
-    const response = (await axios.get(msgUrl)).data;
-
-
-    const mensagem = {
-      id: response.data.id,
-      titulo: response.data.titulo,
-      base64Img: response.data.mensagem,
-      dataExpiracao: response.data.dataExpiracao
-    }
-
-    //***** FUNÇÃO QUE COMPARA DATA ATUAL COM DATA DE EXPIRAÇÃO (RETORNA TRUE OU FALSE) ******/
+    const feriadosUrl = `${process.env.API_HOST}feriados/`;
+    // FUNÇÃO QUE VERIFICA SE A ÚLTIMA MENSAGEM DO BANCO ESTÁEXPIRADA
     const isMessageExpired = (expirationDate) => {
       const actualDate = new Date();
       const actualDateInSeconds = Date.parse(actualDate);
-
       const expirationDateInSeconds = Date.parse(expirationDate);
-
-      console.log(actualDateInSeconds);
-      console.log(expirationDateInSeconds);
-
-      if (expirationDateInSeconds > actualDateInSeconds) {
-        return true;
-      } else {
-        return false
-      }
+      expirationDateInSeconds > actualDateInSeconds
+        ? true
+        : false;
     }
+    // BLOCO UTILIZANDO ASYNC AWAIT
+    try {
+      const msgResponse = await (await axios.get(msgUrl)).data
+      const mensagem = {
+        id: msgResponse.data.id,
+        titulo: msgResponse.data.titulo,
+        base64Img: msgResponse.data.mensagem,
+        dataExpiracao: msgResponse.data.dataExpiracao,
+        expirada: isMessageExpired(msgResponse.data.dataExpiracao)
+      }
 
+      const responseFeriados = await (await axios.get(feriadosUrl)).data;
+      // TRANSFORMANDO VALORES EM VALORES COMPATÍVEIS COM O FULL-CALLENDAR
+      const feriados = await responseFeriados.data.map(feriado => {
+        return {
+          id: feriado.id,
+          title: feriado.titulo,
+          start: feriado.dia,
+          startModal: moment(feriado.dia).format('DD/MM/YYYY'),
+          endModal: feriado.descricao,
+          color: "#34d3",
+          title2: feriado.titulo
+        }
+      });
+      // VERIFICA SE A MENSAGEM ESTÁ EXPIRADA
+      // SE ESTIVER, ENVIA APENAS OS FERIADOS,
+      // CASO NÃO TENHA EXPIRADO, ENVIA AMBOS
+      console.log(feriados)
+      mensagem.expirada
+        ? res.render("public/index", { feriados })
+        : res.render("public/index", { mensagem, feriados })
 
-
-    if (isMessageExpired(mensagem.dataExpiracao)) {
-      return res.render("public/index", { mensagem })
-    } else {
-      return res.render("public/index")
+    } catch (e) {
+      console.log(e);
     }
   });
 
