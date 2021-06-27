d71f9c89435a101e8677fc3e08aa64a0f2042ba3
Marcelo Penha <niqcelo@gmail.com>
2021-06-26 22:37:43 -0300

    remove sockio

--- app.js
+++ app.js
@@ -4,39 +4,29 @@ const http = require('http').Server(app);
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
-const io = require('socket.io')(http);
 
 
-
-app.use((req, res, next) => {
-    req.io = io;
-    return next();
-});
 
 app.use(cookieParser());
 app.use(session({
-    secret: "2C44-4D44-WppQ38S"
+  secret: "2C44-4D44-WppQ38S"
 }));
 
 http.listen(process.env.SERVER_PORT, function () {
-    console.log("Servidor Ativo!");
+  console.log("Servidor Ativo!");
 });
 
 app.get('/', (req, res) => {
 
 
-    console.log('RESPONDENDO AO ROUTER');
-    if (!req.session.token) {
-        res.redirect('/app/login');
-    } else {
-        res.format({
-            html: function () {
-                res.render('template/index', {informacoes: req.session.json});
-            }
-        });
-    }
-});
-
-app.get('*', function(req, res) {
-    res.redirect('/');
+  console.log('RESPONDENDO AO ROUTER');
+  if (!req.session.token) {
+    res.redirect('/app/login');
+  } else {
+    res.format({
+      html: function () {
+        res.render('template/index', { informacoes: req.session.json });
+      }
+    });
+  }
 });
